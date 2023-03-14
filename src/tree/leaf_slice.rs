495be2af17ed02d2fc5b236e0dcb2f87ff978e91
AhoyISki <ahoyiski@gmail.com>
2023-03-14 00:18:11 -0300

    Updated leaf_slice documentation.

--- src/tree/leaf_slice.rs
+++ src/tree/leaf_slice.rs
@@ -27,25 +27,21 @@ where
         self.0.insert_slice(index, slice);
     }
 
-    /// Inserts `string` at `byte_idx` and splits the resulting string in half,
-    /// returning the right half.
-    ///
-    /// Only splits on code point boundaries and will never split CRLF pairs,
-    /// so if the whole string is a single code point or CRLF pair, the split
-    /// will fail and the returned string will be empty.
-    pub fn insert_slice_split(&mut self, split_idx: usize, slice: &[M]) -> Self {
+    /// Inserts a [`&[M]`][Measurable] at `index` and splits the resulting string
+    /// in half, returning the right half.
+    pub fn insert_slice_split(&mut self, split_index: usize, slice: &[M]) -> Self {
         let tot_len = self.len() + slice.len();
-        let mid_idx = tot_len / 2;
+        let mid_index = tot_len / 2;
 
-        self.0.insert_slice(split_idx, slice);
-        let right = LeafSlice::from_slice(&self[mid_idx..]);
-        self.truncate(mid_idx);
+        self.0.insert_slice(split_index, slice);
+        let right = LeafSlice::from_slice(&self[mid_index..]);
+        self.truncate(mid_index);
 
         self.0.inline_if_possible();
         right
     }
 
-    /// Appends a `&str` to end the of the `NodeText`.
+    /// Appends a `&str` to end the of the [LeafSlice].
     pub fn push_slice(&mut self, slice: &[M]) {
         let len = self.len();
         self.0.insert_slice(len, slice);
@@ -62,31 +58,31 @@ where
         self.insert_slice_split(len, slice)
     }
 
-    /// Drops the text after byte index `byte_idx`.
-    pub fn truncate(&mut self, byte_idx: usize) {
-        self.0.truncate(byte_idx);
+    /// Drops the text after `index`.
+    pub fn truncate(&mut self, index: usize) {
+        self.0.truncate(index);
         self.0.inline_if_possible();
     }
 
-    /// Drops the text before byte index `byte_idx`, shifting the
+    /// Drops the text before `index`, shifting the
     /// rest of the text to fill in the space.
-    pub fn truncate_front(&mut self, byte_idx: usize) {
-        self.0.remove_range(0, byte_idx);
+    pub fn truncate_front(&mut self, index: usize) {
+        self.0.remove_range(0, index);
         self.0.inline_if_possible();
     }
 
-    /// Removes the text in the byte index interval `[byte_start, byte_end)`.
-    pub fn remove_range(&mut self, byte_start: usize, byte_end: usize) {
-        self.0.remove_range(byte_start, byte_end);
+    /// Removes the text in the index interval `[start_index, end_index)`.
+    pub fn remove_range(&mut self, start_index: usize, end_index: usize) {
+        self.0.remove_range(start_index, end_index);
         self.0.inline_if_possible();
     }
 
-    /// Splits the `NodeText` at `byte_idx`.
+    /// Splits the [LeafSlice] at `index`.
     ///
     /// The left part remains in the original, and the right part is
-    /// returned in a new `NodeText`.
-    pub fn split_off(&mut self, byte_idx: usize) -> Self {
-        let other = LeafSlice(self.0.split_off(byte_idx));
+    /// returned in a new [LeafSlice].
+    pub fn split_off(&mut self, index: usize) -> Self {
+        let other = LeafSlice(self.0.split_off(index));
         self.0.inline_if_possible();
         other
     }
@@ -187,7 +183,7 @@ mod inner {
 
     use super::Measurable;
 
-    /// The backing internal buffer type for `NodeText`.
+    /// The backing internal buffer type for [LeafSlice][super::LeafSlice].
     #[derive(Copy, Clone)]
     struct BackingArray<M>([M; MAX_LEN])
     where
@@ -208,7 +204,7 @@ mod inner {
         }
     }
 
-    /// Internal small string for `NodeText`.
+    /// Internal small string for [LeafSlice][super::LeafSlice].
     #[derive(Clone, Default)]
     #[repr(C)]
     pub struct LeafSmallVec<M>
@@ -245,57 +241,48 @@ mod inner {
             &self.buffer
         }
 
-        /// Inserts a [`&[Measurable]`][Measurable] at [`byte_idx`][usize].
-        ///
-        /// Panics on out-of-bounds or of [`byte_idx`][usize] isn't a char boundary.
+        /// Inserts a [`&[Measurable]`][Measurable] at `index`.
         #[inline(always)]
-        pub fn insert_slice(&mut self, byte_idx: usize, slice: &[M]) {
-            // Copy bytes from `slice` into the appropriate space in the
-            // buffer.
-            self.buffer.insert_from_slice(byte_idx, slice);
+        pub fn insert_slice(&mut self, index: usize, slice: &[M]) {
+            // Copy the `slice` into the appropriate space in the buffer.
+            self.buffer.insert_from_slice(index, slice);
         }
 
-        /// Removes text in range `[start_byte_idx, end_byte_idx)`
-        ///
-        /// Panics on out-of-bounds or non-char-boundary indices.
+        /// Removes text in range `[start_index, end_index)`
         #[inline(always)]
-        pub fn remove_range(&mut self, start_byte_idx: usize, end_byte_idx: usize) {
-            assert!(start_byte_idx <= end_byte_idx);
+        pub fn remove_range(&mut self, start_index: usize, end_index: usize) {
+            assert!(start_index <= end_index);
             // Already checked by copy_within/is_char_boundary.
-            debug_assert!(end_byte_idx <= self.len());
+            debug_assert!(end_index <= self.len());
             let len = self.len();
-            let amt = end_byte_idx - start_byte_idx;
+            let amt = end_index - start_index;
 
-            self.buffer.copy_within(end_byte_idx..len, start_byte_idx);
+            self.buffer.copy_within(end_index..len, start_index);
 
             self.buffer.truncate(len - amt);
         }
 
-        /// Removes text after `byte_idx`.
+        /// Removes text after `index`.
         #[inline(always)]
-        pub fn truncate(&mut self, byte_idx: usize) {
+        pub fn truncate(&mut self, index: usize) {
             // Already checked by is_char_boundary.
-            debug_assert!(byte_idx <= self.len());
-            self.buffer.truncate(byte_idx);
+            debug_assert!(index <= self.len());
+            self.buffer.truncate(index);
         }
 
-        /// Splits at `byte_idx`, returning the right part and leaving the
+        /// Splits at `index`, returning the right part and leaving the
         /// left part in the original.
-        ///
-        /// Panics on out-of-bounds or of `byte_idx` isn't a char boundary.
         #[inline(always)]
-        pub fn split_off(&mut self, byte_idx: usize) -> Self {
-            // Already checked by is_char_boundary.
-            debug_assert!(byte_idx <= self.len());
+        pub fn split_off(&mut self, index: usize) -> Self {
+            debug_assert!(index <= self.len());
             let len = self.len();
-            let mut other = LeafSmallVec::with_capacity(len - byte_idx);
-            other.buffer.extend_from_slice(&self.buffer[byte_idx..]);
-            self.buffer.truncate(byte_idx);
+            let mut other = LeafSmallVec::with_capacity(len - index);
+            other.buffer.extend_from_slice(&self.buffer[index..]);
+            self.buffer.truncate(index);
             other
         }
 
-        /// Re-inlines the data if it's been heap allocated but can
-        /// fit inline.
+        /// Re-inlines the data if it's been heap allocated but can fit inline.
         #[inline(always)]
         pub fn inline_if_possible(&mut self) {
             if self.buffer.spilled() && (self.buffer.len() <= self.buffer.inline_size()) {
